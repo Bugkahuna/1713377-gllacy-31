5ebd7f19cd5311ca850791ed668827f8f68e5f41
KochevanovVladimir <pipesmoker2@yandex.ru>
2021-07-07 11:52:04 +0300

    Update feedbackForm.js
    добавил валидацию формы

--- js/feedbackForm.js
+++ js/feedbackForm.js
@@ -1,34 +1,50 @@
-const feedbackForm = document.querySelector(".feedback");
+const feedback = document.querySelector(".feedback");
 const feedbackFormBtn = document.querySelector(".relation-btn");
 const siteShadow = document.querySelector(".multyplay-layer");
-const feedbackFormCloseBtn=document.querySelector(".feedeback_close-btn");
-const feedbackFormName = feedbackForm.querySelector("[name=feedback-name]");
+const feedbackForm = feedback.querySelector(".feedback-form")
+const feedbackFormCloseBtn=feedback.querySelector(".feedeback_close-btn");
+const feedbackFormName = feedback.querySelector("[name=feedback-name]");
+const feedbackFormEmail = feedback.querySelector("[name=feedback-email]");
+
+
 
 feedbackFormBtn.addEventListener("click", function(evt){
     evt.preventDefault();
     siteShadow.classList.add("multyplay-layer_show");
-    feedbackForm.classList.add("feedback_show");
+    feedback.classList.add("feedback_show");
     feedbackFormName.focus();
+});
+
+feedbackForm.addEventListener("submit", function(evt){
+  if(!feedbackFormName.value || !feedbackFormEmail.value) {
+    evt.preventDefault();
+    feedback.classList.remove("feedback_error");
+    feedback.classList.add("feedback_error");
+  }
+
 });
 
 
 feedbackFormCloseBtn.addEventListener("click", function(evt){
   evt.preventDefault();
   siteShadow.classList.remove("multyplay-layer_show");
-  feedbackForm.classList.remove("feedback_show");
+  feedback.classList.remove("feedback_show");
+  feedback.classList.remove("feedback_error");
 });
 
 siteShadow.addEventListener("click", function(evt){
   evt.preventDefault();
   siteShadow.classList.remove("multyplay-layer_show");
-  feedbackForm.classList.remove("feedback_show");
+  feedback.classList.remove("feedback_show");
+  feedback.classList.remove("feedback_error");
 });
 
 window.addEventListener("keydown", function(evt){
-  if(evt.key==="Escape" || evt.key==="Escc") {
-    if(feedbackForm.classList.contains("feedback_show") && siteShadow.classList.contains("multyplay-layer_show")){
+  if(evt.key==="Escape" || evt.key==="Esc") {
+    if(feedback.classList.contains("feedback_show") && siteShadow.classList.contains("multyplay-layer_show")){
       siteShadow.classList.remove("multyplay-layer_show");
-      feedbackForm.classList.remove("feedback_show");
+      feedback.classList.remove("feedback_show");
+      feedback.classList.remove("feedback_error");
     }
   }
 });
